f665ac7cd4052b0d24f7d2395ace9523e08127a0
barakaganai <barakaganai01@gmail.com>
2023-03-10 11:36:21 +0300

    create leftBar.js

---
+++ src/components/leftbar/LeftBar.js
@@ -0,0 +1,207 @@
+import {
+  Bookmark,
+  Chat,
+  Event,
+  Group,
+  HelpOutline,
+  PlayCircleFilledOutlined,
+  RssFeed,
+  School,
+  WorkOutline,
+} from "@mui/icons-material";
+import "./LeftBar.css";
+
+export default function LeftBar() {
+  return (
+    <div className="leftBar">
+      <div className="leftBarWrapper">
+        <ul className="leftBarList">
+          <li className="leftBarListItem">
+            <RssFeed className="leftBarIcon" />
+            <span className="leftBarListItemText">Feed</span>
+          </li>
+          <li className="leftBarListItem">
+            <Chat className="leftBarIcon" />
+            <span className="leftBarListItemText">Chats</span>
+          </li>
+          <li className="leftBarListItem">
+            <PlayCircleFilledOutlined className="leftBarIcon" />
+            <span className="leftBarListItemText">Videos</span>
+          </li>
+          <li className="leftBarListItem">
+            <Group className="leftBarIcon" />
+            <span className="leftBarListItemText">Groups</span>
+          </li>
+          <li className="leftBarListItem">
+            <Bookmark className="leftBarIcon" />
+            <span className="leftBarListItemText">Bookmarks</span>
+          </li>
+          <li className="leftBarListItem">
+            <HelpOutline className="leftBarIcon" />
+            <span className="leftBarListItemText">Questions</span>
+          </li>
+          <li className="leftBarListItem">
+            <WorkOutline className="leftBarIcon" />
+            <span className="leftBarListItemText">Jobs</span>
+          </li>
+          <li className="leftBarListItem">
+            <Event className="leftBarIcon" />
+            <span className="leftBarListItemText">Events</span>
+          </li>
+          <li className="leftBarListItem">
+            <School className="leftBarIcon" />
+            <span className="leftBarListItemText">Courses</span>
+          </li>
+        </ul>
+        <button className="leftBarButton">Show More</button>
+        <hr className="leftBarHr" />
+        <ul className="leftBarFriendList">
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+          <li className="leftBarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="leftBarFriendImg"
+            />
+            <span className="leftBarFriendName">Jane doe</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}

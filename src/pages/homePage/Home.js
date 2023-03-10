bb32a39a1ffa6a12aedc85a4048a7038c39d35bb
barakaganai <barakaganai01@gmail.com>
2023-03-10 17:56:55 +0300

    import leftBar, feed and rightBar

--- src/pages/homePage/Home.js
+++ src/pages/homePage/Home.js
@@ -1,9 +1,18 @@
+import Feed from "../../components/feed/Feed";
 import TopBar from "../../components/topbar/TopBar";
+import LeftBar from "../../components/leftbar/LeftBar";
+import RightBar from "../../components/rightbar/RightBar";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div>
       <TopBar />
+      <div className="homeContainer">
+        <LeftBar />
+        <Feed />
+        <RightBar />
+      </div>
     </div>
   );
 }

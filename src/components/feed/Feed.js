53ffa1199b2cdd4b5415feec153bd78185208b31
barakaganai <barakaganai01@gmail.com>
2023-03-10 20:40:10 +0300

    create feed.js

---
+++ src/components/feed/Feed.js
@@ -0,0 +1,26 @@
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./Feed.css";
+
+export default function Feed() {
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
+}

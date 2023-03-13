3cda79347ce27b23be68e1152bdf9c6fbe51d33c
barakaganai <barakaganai01@gmail.com>
2023-03-13 12:24:59 +0300

    implement dynamic function using map

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,32 +1,40 @@
 import { MoreVert } from "@mui/icons-material";
+import { Users } from "../../dummyData";
 import "./Post.css";
 
-export default function Post() {
+export default function Post({ post }) {
+  console.log(post);
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src="/assets/person/1.jpeg" alt="" className="postImage" />
-            <span className="postUsername">Jane Doe</span>
-            <span className="postDate">1 min ago</span>
+            <img
+              src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
+              alt=""
+              className="postImage"
+            />
+            <span className="postUsername">
+              {Users.filter((u) => u.id === post.userId)[0].username}
+            </span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">hey it's my first text</span>
-          <img src="assets/post/1.jpeg" alt="" className="postImg" />
+          <span className="postText">{post?.desc}</span>
+          <img src={post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img src="/assets/like.png" alt="" className="postBottomLeftImg" />
             <img src="/assets/heart.png" alt="" className="postBottomLeftImg" />
-            <span className="postLikeCounter">40 likes</span>
+            <span className="postLikeCounter">{post.like} likes</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">9 comments</span>
+            <span className="postCommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>

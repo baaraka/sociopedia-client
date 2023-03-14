389d4ee91b63f1ce2a3b1d6c7417a1d0e414f8af
barakaganai <barakaganai01@gmail.com>
2023-03-14 16:46:50 +0300

    implement the logic on rightBar

--- src/components/rightbar/RightBar.js
+++ src/components/rightbar/RightBar.js
@@ -99,7 +99,7 @@ export default function RightBar({ profile }) {
   return (
     <div className="rightBar">
       <div className="rightBarWrapper">
-        <ProfileRightBar />
+        {profile ? <ProfileRightBar /> : <HomeRightBar />}
       </div>
     </div>
   );

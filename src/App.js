3d9d06edac402d15185a9dd7c0d1264095523e43
barakaganai <barakaganai01@gmail.com>
2023-03-14 14:01:18 +0300

    import LoginPage in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Home from "./pages/homePage/Home";
-import Profile from "./pages/profile/Profile";
+import Login from "./pages/loginPage/Login";
+import Profile from "./pages/profilePage/Profile";
 
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Login />
     </div>
   );
 }

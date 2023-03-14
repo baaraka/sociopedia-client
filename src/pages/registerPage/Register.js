6814bbef7b8e9a192c3380d6109f3c7af32b9b42
barakaganai <barakaganai01@gmail.com>
2023-03-14 16:47:06 +0300

    create Register.js

---
+++ src/pages/registerPage/Register.js
@@ -0,0 +1,34 @@
+import "./Register.css";
+
+export default function Register() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Sociopedia</h3>
+          <span className="loginDesc">
+            Connect with friends and and the world around you on Sociopedia
+          </span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input type="name" placeholder="Username" className="loginInput" />
+            <input type="email" placeholder="Email" className="loginInput" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="loginInput"
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className="loginInput"
+            />
+            <button className="loginButton">Sign Up</button>
+            <button className="loginRegisterButton">Log into Account</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

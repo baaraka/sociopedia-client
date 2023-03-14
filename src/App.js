c38c3edc721640470d420a84a670fca1ea026e13
barakaganai <barakaganai01@gmail.com>
2023-03-14 16:46:12 +0300

    return home to app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import Home from "./pages/homePage/Home";
 import Login from "./pages/loginPage/Login";
 import Profile from "./pages/profilePage/Profile";
+import Register from "./pages/registerPage/Register";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Home />
     </div>
   );
 }

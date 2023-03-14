7ed451f4b7fe843e158a97e1bba2578c4721a861
barakaganai <barakaganai01@gmail.com>
2023-03-14 14:00:28 +0300

    create login.js

---
+++ src/pages/loginPage/Login.js
@@ -0,0 +1,31 @@
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Sociopedia</h3>
+          <span className="loginDesc">
+            Connect with friends and and the world around you on Sociopedia
+          </span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input type="email" placeholder="Email" className="loginInput" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="loginInput"
+            />
+            <button className="loginButton">Login</button>
+            <span className="loginForgot">Forgot password?</span>
+            <button className="loginRegisterButton">
+              Create a New Account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

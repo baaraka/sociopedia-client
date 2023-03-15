a0b7f9ff6307bdc0476c72094d4e83894bc4cdb7
barakaganai <barakaganai01@gmail.com>
2023-03-15 12:37:09 +0300

    import react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import "./App.css";
 import Home from "./pages/homePage/Home";
 import Login from "./pages/loginPage/Login";
 import Profile from "./pages/profilePage/Profile";
 import Register from "./pages/registerPage/Register";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 

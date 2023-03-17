9a5164a70d597ad6a6420e2fa92f7bcc91f41f96
barakaganai <barakaganai01@gmail.com>
2023-03-17 22:57:12 +0300

    fetch data from database

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,16 +1,27 @@
-import { Posts } from "../../dummyData";
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./Feed.css";
+import axios from "axios";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("posts/timeline/64032b17889848eb325eb53b");
+      console.log(res);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
+        {/* {Posts.map((p) => (
           <Post key={p.id} post={p} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
